ff30363b5f061c9440cd275a68e79f72b9f226c8
Tjomsaas <mat@monil.com>
2023-12-03 18:39:07 +0100

    fixed syntax

--- hwpe-tb/hw/ips/hwpe-mac-engine/rtl/mac_engine.sv
+++ hwpe-tb/hw/ips/hwpe-mac-engine/rtl/mac_engine.sv
@@ -1,30 +1,3 @@
-/*
- * mac_engine.sv
- * Francesco Conti <f.conti@unibo.it>
- *
- * Copyright (C) 2018-2022 ETH Zurich, University of Bologna
- * Copyright and related rights are licensed under the Solderpad Hardware
- * License, Version 0.51 (the "License"); you may not use this file except in
- * compliance with the License.  You may obtain a copy of the License at
- * http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
- * or agreed to in writing, software, hardware and materials distributed under
- * this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- *
- * The architecture that follows is relatively straightforward; it supports two modes:
- *  - in 'simple_mult' mode, the a_i and b_i streams feed the 32b x 32b multiplier (mult).
- *    The output of the multiplier (64b) is registered in a pipeline stage
- *    (r_mult), which is then shifted by ctrl_i.shift to the right and streamed out as d_o.
- *    There is no control local to the module except for handshakes.
- *  - in 'scalar_prod' mode, the c_i stream is first shifted left by ctrl_i.shift, extended
- *    to 64b and saved in r_acc. Then, the a_i and b_i streams feed the 32b x 32b multiplier
- *    (mult) for ctrl_i.len cycles, controlled by a local counter. The output of mult is 
- *    registered in a pipeline stage (r_mult), whose value is used as input to an accumulator
- *    (r_acc) -- the one which was inited by the shifted value of c_i. At the end of the
- *    ctrl_i.len cycles, the output of r_acc is shifted back to the right by ctrl_i.shift
- *    bits and streamed out as d_o.
- */
 
 import aes_package::*;
 
@@ -51,7 +24,7 @@ module mac_engine
   always_comb
   begin
     if (ctrl_i.enable) begin
-    d_o.data = a_i.data
+    d_o.data = a_i.data;
     d_o.valid = ctrl_i.enable;
     d_o.strb  = '1; // strb is always '1 --> all bytes are considered valid
     end 
