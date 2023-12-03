ed539dac4964224d5782af5d95c04e1da8bd451d
Tjomsaas <mat@monil.com>
2023-12-03 18:45:52 +0100

    Fixed num

--- hwpe-tb/hw/ips/hwpe-mac-engine/rtl/mac_engine.sv
+++ hwpe-tb/hw/ips/hwpe-mac-engine/rtl/mac_engine.sv
@@ -1,4 +1,3 @@
-
 import aes_package::*;
 
 module mac_engine
@@ -24,7 +23,7 @@ module mac_engine
   always_comb
   begin
     if (ctrl_i.enable) begin
-    d_o.data = a_i.data;
+    d_o.data = 32'b01110010100111010101110101001110;
     d_o.valid = ctrl_i.enable;
     d_o.strb  = '1; // strb is always '1 --> all bytes are considered valid
     end 
