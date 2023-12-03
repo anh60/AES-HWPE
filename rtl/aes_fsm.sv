5c80d5efd0f061c10303b979624f9583312d51ff
Tjomsaas <mat@monil.com>
2023-12-03 13:27:26 +0100

    Added control signals and registers + engine signals

--- rtl/aes_fsm.sv
+++ rtl/aes_fsm.sv
@@ -9,8 +9,8 @@ module aes_fsm (
   // ctrl & flags
   output ctrl_streamer_t      streamer_ctrl_o,
   input  flags_streamer_t     streamer_flags_i,
-  //output ctrl_engine_t        ctrl_engine_o,
-  //input  flags_engine_t       flags_engine_i,
+  output ctrl_engine_t        ctrl_engine_o,
+  input  flags_engine_t       flags_engine_i,
   output ctrl_slave_t         slave_ctrl_o,
   input  flags_slave_t        slave_flags_i,
   input  ctrl_regfile_t       reg_file_i,
@@ -33,7 +33,7 @@ module aes_fsm (
 
   // AES FSM: combinational next-state calculation process.
   always_comb
-  begin : fsm_next_state_comb
+  begin : fsm_comb_next_state
     next_state = current_state;
 
     case(current_state)
@@ -73,52 +73,74 @@ module aes_fsm (
 
   // AES FSM: combinational output calculation process.
   always_comb
-  begin : fsm_out_comb
-    slave_ctrl_o = '0;
+  begin : fsm_comb_out
+    // engine
+    ctrl_engine_o.clear   = '0;
+    ctrl_engine_o.start   = '0;
+    ctrl_engine_o.enable  = '1;
+
+    //Streamer
+    streamer_ctrl_cfg.plaintext_source_ctrl.req_start = '0;
+    streamer_ctrl_cfg.chipertext_sink_ctrl.req_start  = '0;
     streamer_ctrl_o = streamer_ctrl_cfg;
+    
+    //Slave peripheral? 
+    slave_ctrl_o = '0;
 
     case(current_state) 
-    
+
+      AES_IDLE: begin 
+        ctrl_engine_o.clear  = 1'b1;
+      end 
+
       AES_STARTING: begin 
+        //Engine start
+        ctrl_engine_o.start  = 1'b1;
+        //Streamer request
+        //Should check if streamer is ready, and if not, should wait until ready...
         streamer_ctrl_o.plaintext_source_ctrl.req_start = 1'b1;
         streamer_ctrl_o.chipertext_sink_ctrl.req_start = 1'b1;
       end 
 
+      AES_WORKING: begin 
+        
+      end
+
       AES_FINISHED: begin 
+        ctrl_engine_o.enable = 1'b0;
         slave_ctrl_o.done = 1'b1;
       end 
     endcase
 
   end
 
-// Initialization of streamer control configuration
-// (Uncomment and complete this section as necessary)
 
-  // Here we bind the register file parameters to the streamer configuration.
-  // `streamer_ctrl_cfg` containext_state the "base" configuration, with null `req_start`.
-  // The FSM copies this base configuration into `streamer_ctrl` and sets
-  // the `req_start` signals when in state DM_STARTING.
- /* always_comb
-  begin
-    streamer_ctrl_cfg = '0;
-    streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.dim_enable_1h = '1;
-    streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.dim_enable_1h  = '1;
-    streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.base_addr = reg_file_i.hwpe_params[0];
-    streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.base_addr  = reg_file_i.hwpe_params[1];
-    streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.tot_len   = reg_file_i.hwpe_params[2];
-    streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.tot_len    = reg_file_i.hwpe_params[2];
-    streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.d0_len    = reg_file_i.hwpe_params[3];
-    streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.d0_stride = reg_file_i.hwpe_params[4];
-    streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.d1_len    = reg_file_i.hwpe_params[5];
-    streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.d1_stride = reg_file_i.hwpe_params[6];
-    streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.d2_stride = reg_file_i.hwpe_params[7];
-    streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.d0_len     = reg_file_i.hwpe_params[8];
-    streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.d0_stride  = reg_file_i.hwpe_params[9];
-    streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.d1_len     = reg_file_i.hwpe_params[10];
-    streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.d1_stride  = reg_file_i.hwpe_params[11];
-    streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.d2_stride  = reg_file_i.hwpe_params[12];
+always_comb
+  begin: fsm_comb_reg
+    //Change the number four to actually represent the size of the register
+    //Plaintext stream
+    streamer_ctrl_o.plaintext_source_ctrl.addressgen_ctrl.trans_size  = 4;
+    streamer_ctrl_o.plaintext_source_ctrl.addressgen_ctrl.line_stride = '0;
+    streamer_ctrl_o.plaintext_source_ctrl.addressgen_ctrl.line_length = 4;
+    streamer_ctrl_o.plaintext_source_ctrl.addressgen_ctrl.feat_stride = '0;
+    streamer_ctrl_o.plaintext_source_ctrl.addressgen_ctrl.feat_length = 1;
+    streamer_ctrl_o.plaintext_source_ctrl.addressgen_ctrl.base_addr   = reg_file_i.hwpe_params[0];
+    streamer_ctrl_o.plaintext_source_ctrl.addressgen_ctrl.feat_roll   = '0;
+    streamer_ctrl_o.plaintext_source_ctrl.addressgen_ctrl.loop_outer  = '0;
+    streamer_ctrl_o.plaintext_source_ctrl.addressgen_ctrl.realign_type = '0;
+    // Chipertext stream 
+    streamer_ctrl_o.chipertext_sink_ctrl.addressgen_ctrl.trans_size  = 4;
+    streamer_ctrl_o.chipertext_sink_ctrl.addressgen_ctrl.line_stride = '0;
+    streamer_ctrl_o.chipertext_sink_ctrl.addressgen_ctrl.line_length = 4;
+    streamer_ctrl_o.chipertext_sink_ctrl.addressgen_ctrl.feat_stride = '0;
+    streamer_ctrl_o.chipertext_sink_ctrl.addressgen_ctrl.feat_length = 1;
+    streamer_ctrl_o.chipertext_sink_ctrl.addressgen_ctrl.base_addr   = reg_file_i.hwpe_params[1];
+    streamer_ctrl_o.chipertext_sink_ctrl.addressgen_ctrl.feat_roll   = '0;
+    streamer_ctrl_o.chipertext_sink_ctrl.addressgen_ctrl.loop_outer  = '0;
+    streamer_ctrl_o.chipertext_sink_ctrl.addressgen_ctrl.realign_type = '0;
+
   end
-*/
+
 
 
 
