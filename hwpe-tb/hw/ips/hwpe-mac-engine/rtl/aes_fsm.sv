58b708c61c24c3664d1580f9901e774cabd89eb6
Tjomsaas <mat@monil.com>
2023-12-04 18:22:41 +0100

    Streamer now 8 bytes

--- hwpe-tb/hw/ips/hwpe-mac-engine/rtl/aes_fsm.sv
+++ hwpe-tb/hw/ips/hwpe-mac-engine/rtl/aes_fsm.sv
@@ -136,10 +136,10 @@ always_comb
   begin: fsm_comb_reg
     //Change the number four to actually represent the size of the register
     //Plaintext stream
-    //streamer_ctrl_cfg = '0;
+    streamer_ctrl_cfg = '0;
     streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.trans_size  = 1;
     streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.line_stride = '0;
-    streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.line_length = 1;
+    streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.line_length = 8;
     streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.feat_stride = '0;
     streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.feat_length = 1;
     streamer_ctrl_cfg.plaintext_source_ctrl.addressgen_ctrl.base_addr   = reg_file_i.hwpe_params[0];
@@ -149,7 +149,7 @@ always_comb
     // Chipertext stream 
     streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.trans_size  = 1;
     streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.line_stride = '0;
-    streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.line_length = 1;
+    streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.line_length = 8;
     streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.feat_stride = '0;
     streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.feat_length = 1;
     streamer_ctrl_cfg.chipertext_sink_ctrl.addressgen_ctrl.base_addr   = reg_file_i.hwpe_params[3];
