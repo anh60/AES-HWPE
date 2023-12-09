654009fe2b42d8e21a265148c1129412b6bbc8d0
Tjomsaas <mat@monil.com>
2023-12-09 18:51:14 +0100

    refactor array size

--- hwpe-tb/sw/tb_hwpe.c
+++ hwpe-tb/sw/tb_hwpe.c
@@ -9,8 +9,8 @@
 #define ENCRYPTION_MEMORY 0x1C010100
 #define DECRYPTION_MEMORY 0x1C010200
 
-volatile int *encrypt_mem_address = (volatile int *)ENCRYPTION_MEMORY;
-volatile int *decrypt_mem_address = (volatile int *)DECRYPTION_MEMORY;
+uint32_t *encrypt_mem_address = (uint32_t *)ENCRYPTION_MEMORY;
+uint32_t *decrypt_mem_address = (uint32_t *)DECRYPTION_MEMORY;
 
 /*// Could also do this!
 uint8_t encryption_memory[50];
@@ -22,44 +22,10 @@ uint32_t aes_key[KEY_BIT_LENGTH / 32] = {
     0x4C48DCCC, 0x20A7140E, 0x945B583D, 0x099B712C,
     0x1E18FA7A, 0x27F02E74, 0xD9415694, 0x5724CA72};
 
-uint8_t plaintext[] = {
-    0x01,
-    0x02,
-    0x03,
-    0x04,
-    0x05,
-    0x06,
-    0x07,
-    0x08,
-    0x09,
-    0x0A,
-    0x0B,
-    0x0C,
-    0x0D,
-    0x0E,
-    0x0F,
-    0x10,
-    0x11,
-    0x12,
-    0x13,
-    0x14,
-    0x15,
-    0x16,
-    0x17,
-    0x18,
-    0x19,
-    0x1A,
-    0x1B,
-    0x1C,
-    0x1D,
-    0x1E,
-    0x1F,
-    0x20,
-    0x21,
-    0x22,
-    0x23,
-    0x24,
-};
+uint32_t plaintext[] = {
+    0x01020304, 0x05060708, 0x090A0B0C, 0x0D0E0F10,
+    0x11121314, 0x15161718, 0x191A1B1C, 0x1D1E1F20,
+    0x21222324};
 
 int main()
 {
